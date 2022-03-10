ac18bfc8be7b3d001e4a340ee71931df87acfc5e
Achitha Sandaruwan <83119823+sandaru1b4@users.noreply.github.com>
2022-03-10 18:27:31 +0530

    SideBar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./Header";
+import SideBar from "./SideBar";
 
 function App() {
   return (
     <div className="App">
       {/* header */}
       <Header />
-
+      
       {/* Side Bar */}
+      <SideBar />
       {/* Feed */}
       {/* Widgets */}
     </div>
---
+++ src/SideBar.js
@@ -0,0 +1,16 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./SideBar.css";
+
+function SideBar() {
+  return <div className="sideBar">
+    <div className="sidebar__top">
+      <img src="" alt=""/>
+      <Avatar/>
+      <h2>User Name</h2>
+      <h5>Description</h5>
+      </div>
+    </div>;
+}
+
+export default SideBar;
